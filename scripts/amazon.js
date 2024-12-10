20202055036c55e9426e278aacd5c5d9289f739f
charan11w <charan11w@gmail.com>
2024-12-10 15:45:55 +0530

    made added text visible

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -40,7 +40,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -55,10 +55,12 @@ products.forEach((product) => {
 
 document.querySelector('.js-html').innerHTML=productHtml;
 
+const addedMessageTimeouts={};
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
   button.addEventListener('click',() => {
-    const productId=button.dataset.productId;
+    const {productId}=button.dataset;
 
     let matchingItem;
 
@@ -75,8 +77,8 @@ document.querySelectorAll('.js-add-to-cart')
       matchingItem.quantity += quantity;
     }else{
       cart.push({
-        productId : productId,
-        quantity : quantity
+        productId,
+        quantity
       })
     }
 
@@ -87,5 +89,22 @@ document.querySelectorAll('.js-add-to-cart')
     })
 
     document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
+
+    const addedMessage=document.querySelector(`.js-added-to-cart-${productId}`)
+
+    addedMessage.classList.add('js-added-visible')
+
+
+    const messageTimeoutId=addedMessageTimeouts[productId];
+
+    if(messageTimeoutId){
+      clearInterval(messageTimeoutId);
+    }
+
+    const timeoutId=setTimeout(() => {
+      addedMessage.classList.remove('js-added-visible')
+    },2000)
+
+    addedMessageTimeouts[productId]=timeoutId;
   })
 })
