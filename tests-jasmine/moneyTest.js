7a6f62bb7b2967c9c0143fa22c7fd471c3a2989f
charan11w <charan11w@gmail.com>
2024-12-19 16:11:46 +0530

    remove default tests and add default tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,15 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite : formatCurrency',() => {
+  it('converts cents into dollers',() => {
+    expect(formatCurrency(2095)).toBe('20.95');
+  });
+
+  it('rounds to zero',() => {
+    expect(formatCurrency(0)).toBe('0.00');
+  });
+
+  it('round to the nearest value',() => {
+    expect(formatCurrency(2000.5)).toBe('20.01');
+  });
+});
