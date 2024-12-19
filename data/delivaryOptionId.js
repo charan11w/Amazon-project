6edfaca630f61a04885c14ec34028c38a5df7d85
charan11w <charan11w@gmail.com>
2024-12-19 14:18:15 +0530

    updated delivery dates to skip weekends

--- data/delivaryOptionId.js
+++ data/delivaryOptionId.js
@@ -31,14 +31,28 @@ export function getDelivaryOption(delivaryOptionId){
   return delivaryOption;
 }
 
-export function calculateDelivaryDate(delivaryOption){
-  const today=dayjs();
-  const delivaryDate=today.add(
-    delivaryOption.delivaryDays,'days'
-  );
-  const dateString=delivaryDate.format(
-    'dddd , MMMM D'
-  );
+function isWeekEnd(date){
+  const dayOfWeek=date.format('dddd');
+  return dayOfWeek === 'Saturday' || dayOfWeek === 'Sunday';
+}
 
+
+export function calculateDelivaryDate(delivaryOption){
+  let remainingDays=delivaryOption.delivaryDays;
+  let deliveryDate=dayjs();
+
+  while (remainingDays > 0) {
+    deliveryDate = deliveryDate.add(1, 'day');
+
+    if (!isWeekEnd(deliveryDate)) {
+      remainingDays--;
+      // This is a shortcut for:
+      // remainingDays = remainingDays - 1;
+    }
+  }
+
+  const dateString = deliveryDate.format(
+    'dddd, MMMM D');
+  
   return dateString;
 }
