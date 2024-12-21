9f3dc1c0ee3a826cc8bf9be4e52bdb11accb876d
charan11w <charan11w@gmail.com>
2024-12-21 12:46:23 +0530

    create oops version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummay } from "./checkout/paymentSummary.js";
 import { renderCheckoutHtml } from './checkout/checkoutHeader1.js';
+import '../data/cart-oop.js';
 
 renderCheckoutHtml();
 renderOrderSummary();
